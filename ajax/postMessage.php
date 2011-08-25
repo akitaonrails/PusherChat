02f74a155a7c42af5288e82df46487532e22a3d2
AkitaOnRails <fabioakita@gmail.com>
2011-08-25 12:00:10 -0300

    Fixing the Pusher php consumer.

--- ajax/postMessage.php
+++ ajax/postMessage.php
@@ -2,17 +2,17 @@
 
 	require_once('../config.php');
 	require('Pusher.php');
-	
+
 	$message = $_POST['message'];
 	$nickname = $_POST['nickname'];
 	$channel = $_POST['chatChannel'];
-	
+
 	$content = array(
 		"content"=>$message,
 		"nickname"=>$nickname
 	);
 
-	$pusher = new Pusher(PUSHER_API_KEY, PUSHER_API_SECRET, PUSHER_APP_ID, $channel);
-	$pusher->trigger('message', $content);
+	$pusher = new Pusher(PUSHER_API_KEY, PUSHER_API_SECRET, PUSHER_APP_ID, true);
+	$pusher->trigger($channel, 'message', $content);
 
 ?>
